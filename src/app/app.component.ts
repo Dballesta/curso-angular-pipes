a86eafdc8053f8a16867a3797ab51918699dbebc
Daniel <danielballesteros4@gmail.com>
2019-08-15 21:30:49 +0200

    Seccion 77 Json Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Daniel';
+
+  arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  porciento = 0.23;
+
+  dinero = 55436.4122;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      numero: '12'
+    }
+  };
 }
